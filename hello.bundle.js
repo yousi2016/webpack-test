c172447d67ea1e2123d08d4d2ee1659e77f5bbb2
yousi2016 <dingyousi888@gmail.com>
2018-01-07 18:39:34 +0800

    add callback

--- hello.bundle.js
+++ hello.bundle.js
@@ -70,13 +70,18 @@
 /*
 * @Author: Administrator
 * @Date:   2018-01-04 12:02:00
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-01-06 17:15:52
+* @Last Modified by:   yousi2016
+* @Last Modified time: 2018-01-07 18:16:16
 */
 __webpack_require__(1);
 
 function hello (str) {
     alert(str);
+};
+function callback (){
+    return function(){
+        
+    }
 };
 
 /***/ }),
--- hello.js
+++ hello.js
@@ -1,11 +1,16 @@
 /*
 * @Author: Administrator
 * @Date:   2018-01-04 12:02:00
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-01-06 17:15:52
+* @Last Modified by:   yousi2016
+* @Last Modified time: 2018-01-07 18:16:16
 */
 require('./world.js');
 
 function hello (str) {
     alert(str);
+};
+function callback (){
+    return function(){
+        
+    }
 };

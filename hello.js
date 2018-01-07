dbc20c29e0e5303facf852d3ae4d3a97754c1fbb
yousi2016 <dingyousi888@gmail.com>
2018-01-07 09:38:29 +0800

    add a webpack-test project

---
+++ hello.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2018-01-04 12:02:00
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-01-06 17:15:52
+*/
+require('./world.js');
+
+function hello (str) {
+    alert(str);
+};
---
+++ world.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2018-01-05 22:51:25
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-01-06 17:15:49
+*/
+function world () {
+    return {
+
+    };
+};

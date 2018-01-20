3c1c723284ddb38695cb62d216243212cc7f35c8
yousi2016 <dingyousi888@gmail.com>
2018-01-20 11:07:05 +0800

    index.html/style.css

--- hello.js
+++ hello.js
@@ -2,13 +2,17 @@
 * @Author: Administrator
 * @Date:   2018-01-04 12:02:00
 * @Last Modified by:   yousi2016
-* @Last Modified time: 2018-01-07 18:16:16
+* @Last Modified time: 2018-01-13 10:03:21
 */
 require('./world.js');
+
+require('./style.css');
+
 
 function hello (str) {
     alert(str);
 };
+
 function callback (){
     return function(){
         
